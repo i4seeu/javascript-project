910fadc44095b579a634a4c06b9970e010b8fcbb
Noordeen Malango <noordinmalango@gmail.com>
2019-08-26 14:58:35 +0200

    conditional statements

--- js/custom.js
+++ js/custom.js
@@ -9,8 +9,6 @@ var name = "Noordeen Malango";
 console.log(name);
 console.log(name);
 
-var age = 18 +3;
-console.log(age);
 
 var company_address = "private bag 360, chichiri Blantyre";
 var personal_address = "p.o box 287, Chilomoni, Balntyre";
@@ -24,3 +22,42 @@ var firstname = "vanessa";
 var surname = "Narciso";
 console.log(firstname);
 console.log(surname);
+
+// control structures
+var age = 10;
+//  1 == 2 false, 2 == 2 true, 2 >= 3 false, 4 >= 2 true,
+// 1 <= 2 true, 3 <= 2 false, 3 != 4 true, 4 !=4 false
+if(age >=13)
+{
+  console.log("You are legible to access this site");
+}
+else
+{
+  console.log("You are illegible to access the site");
+}
+// else if block
+var mark = 80;
+
+if(mark >=70)
+{
+   console.log("A");
+}
+else if (mark >=60) {
+  console.log("B");
+}
+else if (mark >50) {
+  console.log("C");
+}
+else if (mark >40) {
+  console.log("D");
+}
+else{
+  console.log("F");
+}
+
+
+ // QUESTION:
+/*
+  declare 2 variables weight and age , weight index (weight/age) e.g 2
+  you check if weigh index > 3  , console obesity, >=2 normal , <2 you are  slim
+*/
