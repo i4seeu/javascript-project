59c1fe5d132cff9dc60ef8885d8b1dbf7031f79a
Noordeen Malango <noordinmalango@gmail.com>
2019-08-26 11:30:18 +0200

    introduction to javascrtipt

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+console.log("Introduction to Javascript");
+// console.log("Noordeen"); single line comment
+console.log("Multiple line comment");
+/*
+This is the world of programming and is fun
+*/
+// = assignment operator
+var name = "Noordeen Malango";
+console.log(name);
+console.log(name);
+
+var age = 18 +3;
+console.log(age);
+
+var company_address = "private bag 360, chichiri Blantyre";
+var personal_address = "p.o box 287, Chilomoni, Balntyre";
+console.log(company_address);
+console.log(personal_address);
+
+// Try this out vanessa , declare two variables that will keep your firstname , and surname
+// print them to the console
