2059ad6dd1cddebf6fd008e8412637f48b69300e
VN-7 <vnarciso@softhacksai.com>
2019-08-28 11:44:24 +0200

    loops

--- js/loops.js
+++ js/loops.js
@@ -20,3 +20,18 @@ for(initialize; condition; increment){
 assignment : write a code that calculates the sum of numbers from 1 to 100
 
 */
+
+function sum(num1,num2){
+var total = num1 + num2;
+return total;
+}
+
+var addition = 0;
+var num = 1;
+
+while(num <= 1000){
+  addition = sum(addition, num);
+  num++;
+  /*console.log(addition); if this displayed here will show all calculations*/
+}
+console.log(addition); //this displays only the final calculation
